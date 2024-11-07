8fc37d32142b361fbe82e1133dd1a937f42c6d28
namanjaink <naman.jain4@ltimindtree.com>
2024-11-07 18:04:59 +0530

    New Product add component

---
+++ Demo/src/app/shared/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppComponent } from "../../app.component";
+import { RouterLink } from '@angular/router';
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [AppComponent, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+
+}
