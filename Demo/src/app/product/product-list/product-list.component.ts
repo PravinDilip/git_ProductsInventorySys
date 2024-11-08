dd7a5c98eb7704e5c274182f5d3bf099be1046ba
namanjaink <naman.jain4@ltimindtree.com>
2024-11-08 17:43:47 +0530

    Update Functionalty Changes

--- Demo/src/app/product/product-list/product-list.component.ts
+++ Demo/src/app/product/product-list/product-list.component.ts
@@ -2,35 +2,39 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ProductServiceService } from '../../service/product-service.service';
 import { Product } from '../../product';
 import { CommonModule, NgFor } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [CommonModule,NgFor],
+  imports: [CommonModule, NgFor],
   templateUrl: './product-list.component.html',
-  styleUrl: './product-list.component.scss'
+  styleUrl: './product-list.component.scss',
 })
 export class ProductListComponent implements OnInit {
- public productDetailsList:Product[]=[];
- service = inject(ProductServiceService);
-  constructor(){
-
-  }
+  public productDetailsList: Product[] = [];
+  service = inject(ProductServiceService);
+  router = inject(Router);
+  constructor() {}
 
   ngOnInit(): void {
     this.getAllProductDetailsData();
   }
 
-  getAllProductDetailsData(){
-    this.service.getAllProducts().subscribe((data:Product[])=>{
-        this.productDetailsList= data
-    },
-  )
+  getAllProductDetailsData() {
+    this.service.getAllProducts().subscribe((data: Product[]) => {
+      this.productDetailsList = data;
+    });
   }
-  deleteProduct(id:any){
-    this.service.deleteProduct(id).subscribe(res=>{
-      this.productDetailsList = this.productDetailsList.filter(item=> item.id !== id);
-    })
+  deleteProduct(id: any) {
+    this.service.deleteProduct(id).subscribe((res) => {
+      this.productDetailsList = this.productDetailsList.filter(
+        (item) => item.id !== id
+      );
+    });
   }
-
+  updateProduct(productId: any) {
+    console.log( this.router.navigate([`/product/${productId}`]));
+    this.router.navigate([`/product/${productId}`]);
+  }
 }
