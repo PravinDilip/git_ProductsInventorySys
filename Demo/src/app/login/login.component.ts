ef94c51caf46454289f2d8cb8d7dd448ea71f358
PravinDilip <pravindilip.10748253@ltimindtree.com>
2025-02-26 13:39:21 +0530

    login form with reactive form created

---
+++ Demo/src/app/login/login.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import{ReactiveFormsModule} from '@angular/forms';
+import {MatCardModule} from '@angular/material/card';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import { FormArray,FormBuilder,FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatFormFieldModule,MatButtonModule, MatInputModule, MatSelectModule,CommonModule,ReactiveFormsModule,MatCardModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss'
+})
+
+export class LoginComponent implements OnInit{
+
+  loginform:any;
+  constructor(public formBuilder:FormBuilder){}
+
+  ngOnInit(): void {
+    this.loginform =this.formBuilder.group({
+      username:[''],
+      password:['']
+    })
+  }
+
+  onSave(values:any){
+    console.log('data',values);
+  }
+
+}
