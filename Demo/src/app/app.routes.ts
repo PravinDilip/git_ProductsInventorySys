5e11037cd5f7e870e7374812d0d531bd2c540901
PravinDilip <pravindilip.10748253@ltimindtree.com>
2025-02-27 13:21:42 +0530

    flight details page created

--- Demo/src/app/app.routes.ts
+++ Demo/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { FlightsComponent } from './flights/flights.component';
+import { FdetailsComponent } from './fdetails/fdetails.component';
 
 export const routes: Routes = [{
     path:'',
@@ -10,6 +11,6 @@ export const routes: Routes = [{
       component:FlightsComponent 
     },{
       path:'flights/details',
-      component:FlightsComponent
+      component:FdetailsComponent
     }
 ];
---
+++ Demo/src/app/fdetails/fdetails.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
+
+@Component({
+  selector: 'app-fdetails',
+  standalone: true,
+  imports: [MatCardModule, MatChipsModule, MatProgressBarModule,CommonModule,MatListModule,MatInputModule,FormsModule,MatCheckboxModule,MatListModule],
+  templateUrl: './fdetails.component.html',
+  styleUrl: './fdetails.component.scss'
+})
+export class FdetailsComponent {
+  
+  typesOfShoes: string[] = ['Boots -seat no:12345', 'Clogs -seat no:12345', 'Loafers -seat no:12345', 'Moccasins -seat no:12345', 'Sneakers -seat no:12345','Boots -seat no:12345', 'Clogs -seat no:12345', 'Loafers -seat no:12345', 'Moccasins -seat no:12345', 'Sneakers -seat no:12345'];
+  checked = false;
+  indeterminate = true;
+
+}
